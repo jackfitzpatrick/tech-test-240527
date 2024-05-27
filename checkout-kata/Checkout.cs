31d9db30662e1257444e93155304ae03058e4887
JackFitzpatrick <fitzgothic@gmail.com>
2024-05-27 22:25:11 +0100

    feat: refactors to handle item a special pricing

--- checkout-kata/Checkout.cs
+++ checkout-kata/Checkout.cs
@@ -2,13 +2,32 @@ public class Checkout : ICheckout
 {
   public int GetTotalPrice()
   {
+    var itemAs = Items.FindAll(x => x.StockKeepingUnit == "A");
+    Items.RemoveAll(x => x.StockKeepingUnit == "A");
+    var numberOfSpecialPrices = (int)((double)itemAs.Count / 3);
+    var specialPriceTotal = 130 * numberOfSpecialPrices;
+    TotalPrice = TotalPrice += specialPriceTotal;
+    TotalPrice = TotalPrice += itemAs.Count * 50;
+
+    foreach (var item in Items)
+    {
+      TotalPrice += item.UnitPrice;
+    }
+
     return TotalPrice;
   }
 
   public void Scan(Item item)
   {
-    TotalPrice += item.UnitPrice;
+    Items.Add(item);
   }
 
+  private List<Item> Items;
+
   private int TotalPrice;
+
+  public Checkout()
+  {
+    Items = [];
+  }
 }
