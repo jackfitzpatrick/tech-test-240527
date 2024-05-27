9b79bf5ead587f5aa5ab9ad180bdace92eba29b8
JackFitzpatrick <fitzgothic@gmail.com>
2024-05-27 21:39:31 +0100

    chore: adds tests for scanning each item type and static class of Items

--- checkout-kata.tests/CheckoutTests.cs
+++ checkout-kata.tests/CheckoutTests.cs
@@ -1,7 +1,8 @@
 namespace checkout_kata.tests;
-
+[TestFixture]
 public class CheckoutTests
 {
+
   [Test]
   public void IsNotNull()
   {
@@ -10,17 +11,56 @@ public class CheckoutTests
   }
 
   [Test]
-  public void CanScanItem()
+  public void CanScanItemA()
   {
     var checkout = new Checkout();
-    var item = new Item
-    {
-      StockKeepingUnit = "A",
-      UnitPrice = 50,
-    };
 
-    checkout.Scan(item);
+    checkout.Scan(SupermarketItems.itemA);
     var actual = checkout.GetTotalPrice();
     Assert.AreEqual(50, actual);
+  }
+
+  [Test]
+  public void CanScanItemB()
+  {
+    var checkout = new Checkout();
+
+    checkout.Scan(SupermarketItems.itemB);
+    var actual = checkout.GetTotalPrice();
+    Assert.AreEqual(30, actual);
+  }
+
+  [Test]
+  public void CanScanItemC()
+  {
+    var checkout = new Checkout();
+
+    checkout.Scan(SupermarketItems.itemC);
+    var actual = checkout.GetTotalPrice();
+    Assert.AreEqual(20, actual);
+  }
+
+  [Test]
+  public void CanScanItemD()
+  {
+    var checkout = new Checkout();
+
+    checkout.Scan(SupermarketItems.itemD);
+    var actual = checkout.GetTotalPrice();
+    Assert.AreEqual(15, actual);
+  }
+
+  [Test]
+  public void CanScanMultipleItems()
+  {
+    var checkout = new Checkout();
+
+    checkout.Scan(SupermarketItems.itemA);
+    checkout.Scan(SupermarketItems.itemB);
+    checkout.Scan(SupermarketItems.itemC);
+    checkout.Scan(SupermarketItems.itemD);
+
+    var actual = checkout.GetTotalPrice();
+    Assert.AreEqual(115, actual);
   }
 }
---
+++ checkout-kata/SupermarketItems.cs
@@ -0,0 +1,24 @@
+//Would most likely be in DB
+public static class SupermarketItems
+{
+  public static Item itemA = new Item
+  {
+    StockKeepingUnit = "A",
+    UnitPrice = 50,
+  };
+  public static Item itemB = new Item
+  {
+    StockKeepingUnit = "B",
+    UnitPrice = 30,
+  };
+  public static Item itemC = new Item
+  {
+    StockKeepingUnit = "C",
+    UnitPrice = 20,
+  };
+  public static Item itemD = new Item
+  {
+    StockKeepingUnit = "D",
+    UnitPrice = 15,
+  };
+}
